9f0d8d1cd68fc1e71e0142475e2a91f7ffe5a8bf
Lisa Gee <zhusufeng@gmail.com>
2017-10-19 21:23:28 -0700

    Create submit function

--- client/app/controllers/mainCtrl.js
+++ client/app/controllers/mainCtrl.js
@@ -1,4 +1,11 @@
 angular.module('index')
  .controller('mainCtrl', function() {
-   
+
+  this.submit = function() {
+    let currentMileage = document.getElementById('currentMileage').value;
+    let gallons = document.getElementById('gallons').value;
+    let pricePerGallon = document.getElementById('pricePerGallon').value;
+    // document.getElementById('gasForm').submit();
+    console.log('You submitted currentMileage: ' + currentMileage + ', gallons: ' + gallons + ', price per gallon: ' + pricePerGallon);
+  };
  });
