984edfe47c6ac115e0921bb14842e9e2818c0388
Lisa Gee <zhusufeng@gmail.com>
2017-10-26 19:52:43 -0700

    Add previousMileage to submit

--- client/app/controllers/mainCtrl.js
+++ client/app/controllers/mainCtrl.js
@@ -2,10 +2,11 @@ angular.module('index')
  .controller('mainCtrl', function() {
 
   this.submit = function() {
+    let previousMileage = document.getElementById('previousMileage').value;
     let currentMileage = document.getElementById('currentMileage').value;
     let gallons = document.getElementById('gallons').value;
     let pricePerGallon = document.getElementById('pricePerGallon').value;
     // document.getElementById('gasForm').submit();
-    console.log('You submitted currentMileage: ' + currentMileage + ', gallons: ' + gallons + ', price per gallon: ' + pricePerGallon);
+    console.log('You submitted previousMileage: ' + previousMileage + ', currentMileage: ' + currentMileage + ', gallons: ' + gallons + ', price per gallon: ' + pricePerGallon);
   };
  });
