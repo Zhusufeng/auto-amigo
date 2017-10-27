970968011501ed59e62b8bc695ecee4fbda191fd
Lisa Gee <zhusufeng@gmail.com>
2017-10-26 22:02:50 -0700

    Pseudocode MPG calculations

--- client/app/controllers/mainCtrl.js
+++ client/app/controllers/mainCtrl.js
@@ -6,7 +6,14 @@ angular.module('index')
     let currentMileage = document.getElementById('currentMileage').value;
     let gallons = document.getElementById('gallons').value;
     let pricePerGallon = document.getElementById('pricePerGallon').value;
-    // document.getElementById('gasForm').submit();
     console.log('You submitted previousMileage: ' + previousMileage + ', currentMileage: ' + currentMileage + ', gallons: ' + gallons + ', price per gallon: ' + pricePerGallon);
+
+    // Calculate 
+    let milesDriven = previousMileage - currentMileage; // Round to 2 decimals
+    let MPG = milesDriven/gallons; // Round to 2 decimals
+    let totalSpent = gallons * pricePerGallon; // Round to 2 decimals
+
+    // Append a row to table
+
   };
  });
