89bd07cc24d09b52567cfd46ebed5a547b2b43f8
Lisa Gee <zhusufeng@gmail.com>
2017-10-26 22:35:49 -0700

    Create this.gaslog and add new entries to it

--- client/app/controllers/mainCtrl.js
+++ client/app/controllers/mainCtrl.js
@@ -1,6 +1,19 @@
 angular.module('index')
  .controller('mainCtrl', function() {
 
+  this.gaslog = [
+    {
+      date: '10/19/17',
+      previousMileage: 99000,
+      currentMileage: 99500,
+      milesDriven: 500,
+      gallons: 15,
+      MPG: 33.33,
+      pricePerGallon: 3.25,
+      totalSpent: 48.75
+    }
+  ];
+
   this.submit = function() {
     let previousMileage = document.getElementById('previousMileage').value;
     let currentMileage = document.getElementById('currentMileage').value;
@@ -9,11 +22,25 @@ angular.module('index')
     console.log('You submitted previousMileage: ' + previousMileage + ', currentMileage: ' + currentMileage + ', gallons: ' + gallons + ', price per gallon: ' + pricePerGallon);
 
     // Calculate 
-    let milesDriven = previousMileage - currentMileage; // Round to 2 decimals
-    let MPG = milesDriven/gallons; // Round to 2 decimals
-    let totalSpent = gallons * pricePerGallon; // Round to 2 decimals
+    let milesDriven = (currentMileage - previousMileage).toFixed(0);
+    let MPG = (milesDriven/gallons).toFixed(2); 
+    let totalSpent = (gallons * pricePerGallon).toFixed(2); 
 
+    console.log(milesDriven, MPG, totalSpent);
+    
     // Append a row to table
 
+    this.gaslog.push(
+      {
+        date: '10/26/17',
+        previousMileage: previousMileage,
+        currentMileage: currentMileage,
+        milesDriven: milesDriven,
+        gallons: gallons,
+        MPG: MPG,
+        pricePerGallon: pricePerGallon,
+        totalSpent: totalSpent
+      }
+    );
   };
  });
