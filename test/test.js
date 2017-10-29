b9eb0ef9e341f7f9979997d080601733934ed1b4
Lisa Gee <zhusufeng@gmail.com>
2017-10-29 00:26:13 -0700

    Add second test with Chai assert

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 var assert = require('assert');
-describe('My first test', function() {
+
+describe('My first test with Mocha framework only', function() {
   describe('Simple data types with the same value', function() {
     it('should equal each other', function() {
       assert.equal(6, 6);
+    });
+  });
+});
+
+describe('My second test with Chai library', function() {
+  describe('Complex data types with the same value', function() {
+    it('should deep equal each other', function() {
+      assert.deepEqual({ tea: 'green' }, { tea: 'green' });
     });
   });
 });
