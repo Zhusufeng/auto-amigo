cbead21e04280c07606fae684b7515faf63c1241
Lisa Gee <zhusufeng@gmail.com>
2017-10-30 21:30:52 -0700

    Update gasCtrl test to use inject

--- test/test.js
+++ test/test.js
@@ -1,32 +1,40 @@
 var assert = require('assert');
 
-// describe('My first test with Mocha framework only', function() {
-//   describe('Simple data types with the same value', function() {
-//     it('should equal each other', function() {
-//       assert.equal(6, 6);
-//     });
-//   });
-// });
+describe('My first test with Mocha framework only', function() {
+  describe('Simple data types with the same value', function() {
+    it('should equal each other', function() {
+      assert.equal(6, 6);
+    });
+  });
+});
 
-// describe('My second test with Chai library', function() {
-//   describe('Complex data types with the same value', function() {
-//     it('should deep equal each other', function() {
-//       assert.deepEqual({ tea: 'green' }, { tea: 'green' });
-//     });
-//   });
-// });
+describe('My second test with Chai library', function() {
+  describe('Complex data types with the same value', function() {
+    it('should deep equal each other', function() {
+      assert.deepEqual({ tea: 'green' }, { tea: 'green' });
+    });
+  });
+});
 
-describe('gas', function() {
+describe('gasCtrl', function() {
 
-  // Define variables
-  var index;
+  // Define variables we want access to
+  var gasCtrl;
 
   beforeEach(function() {
-    module('index'));
+    // Load the module
+    module('index');
+
+    // Inject
+    inject(function($controller) {
+      gasCtrl = $controller('gasCtrl');
+      console.log(gasCtrl);
+    });
   });
 
-  it('should have an array that will keep the gas log history', function() {
-    assert.exists(index.gaslog);
-  });
-
+  describe('controller', function() {
+    it('should have an array that will keep the gas log history', function() {
+      assert.exists(index.gaslog);
+    });
+  })
 });
