ad0526b4d38185a644b93344e99cbb7724c3184d
Lisa Gee <zhusufeng@gmail.com>
2017-11-16 10:53:15 -0800

    Handle reqs to /user with simple res

--- server/server.js
+++ server/server.js
@@ -8,4 +8,22 @@ app.use(bodyParser.json());
 // Load static assets from client folder
 app.use(express.static('client'));
 
+app.route('/user')
+  .get((req, res) => {
+    res.status(200).send('You made a GET to the /user endpoint');
+  })
+  .post((req, res) => {
+    res.status(200).send('You made a POST to the /user endpoint');
+  });
+
+app.route('/user/:id')
+  .get((req, res) => {
+    res.status(200).send('You made a GET to the /user/:id endpoint');
+  })
+  .put((req, res) => {
+    res.status(200).send('You made a PUT to the /user/:id endpoint');
+  })
+  .delete((req, res) => {
+    res.status(200).send('You made a DELETE to the /user/:id endpoint');
+  });
 module.exports = app;
