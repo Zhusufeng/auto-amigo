6415e818b56f84149a41f3d3987361026fbaa863
Lisa Gee <zhusufeng@gmail.com>
2017-11-22 11:53:10 -0800

    Add simple functionality to GET for gasService

--- client/app/services/gasService.js
+++ client/app/services/gasService.js
@@ -1,4 +1,12 @@
 angular.module('index')
   .service('gasService', function($http) {
-    
+    const getGas = $http({
+      method: 'GET',
+      url: '/gas'
+    })
+    .then(function(res) {
+      console.log('Success!', res.data);
+    }, function(err) {
+      console.log('Error!', err);
+    });
   });
