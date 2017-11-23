e0e15e17d2db943a805942472cd03986fb0f44d5
Lisa Gee <zhusufeng@gmail.com>
2017-11-23 09:44:55 -0800

    Create get request that runs when needed

--- client/app/services/gasService.js
+++ client/app/services/gasService.js
@@ -1,12 +1,25 @@
 angular.module('index')
-  .service('gasService', function($http) {
-    const getGas = $http({
-      method: 'GET',
-      url: '/gas'
-    })
-    .then(function(res) {
-      console.log('Success!', res.data);
-    }, function(err) {
-      console.log('Error!', err);
-    });
+  .service('gasService', function($http, $q) {
+    // const getGas = $http({
+    //   method: 'GET',
+    //   url: '/gas'
+    // })
+    // .then(function(res) {
+    //   console.log('Success!', res.data);
+    // }, function(err) {
+    //   console.log('Error!', err);
+    // });
+
+    this.getGasData = function(){
+      var temp = {};
+      var defer = $q.defer();
+      $http.get('/gas')
+        .success(data => {
+          console.log('Success from getGasData!', data);
+          temp = data;
+          defer.resolve(data);
+        });
+      return defer.promise;
+    };
+
   });
