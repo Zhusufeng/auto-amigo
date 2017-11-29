1c264f3ae358a6789b2c882c66fc3409eb94a512
Lisa Gee <zhusufeng@gmail.com>
2017-11-29 10:08:24 -0800

    Add comments to clarify and separate routes

--- server/routes/gas.js
+++ server/routes/gas.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Gas = require('./db/models/gas');
 
+// Temp array until db is used
 let gaslog = [
   {
     date: '10/19/2017',
@@ -14,10 +15,12 @@ let gaslog = [
   }
 ];
 
+/* GET /gas route to retrieve all gas entries */
 const getGas = (req, res) => {
   res.status(200).send(gaslog);
 };
 
+/* POST /gas route to add a gas entry */
 const postGas = (req, res) => {
   gaslog.push(req.body);
   res.status(200).send(req.body);
