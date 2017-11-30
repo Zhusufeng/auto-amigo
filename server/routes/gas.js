514c9ba768bf074866e374f38be1ef8b1df3046f
Lisa Gee <zhusufeng@gmail.com>
2017-11-30 09:16:32 -0800

    Clean up code in routes/gas.js

--- server/routes/gas.js
+++ server/routes/gas.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 const Gas = require('../db/models/gas');
 
-// Temp array until db is used
-// let gaslog = [
-//   {
-//     date: '10/19/2017',
-//     previousMileage: 99000,
-//     currentMileage: 99500,
-//     milesDriven: 500,
-//     gallons: 15,
-//     MPG: 33.33,
-//     pricePerGallon: 3.25,
-//     totalSpent: 48.75
-//   }
-// ];
-
 /* GET /gas route to retrieve all gas entries */
 const getGas = (req, res) => {
   Gas.find(function(err, gasEntries) {
@@ -40,10 +26,8 @@ const postGas = (req, res) => {
   // Save gasEntry to database
   gasEntry.save(function (err, gasEntry) {
     if (err) return console.error(err);
-  });
-
-  // gaslog.push(req.body);
-  res.status(200).send(req.body);
+    res.status(200).send(req.body);
+  }); 
 };
 
 // Export functions using es6
