f09cbc1c38f42b00a53170e89d74eaa4ab84ab10
Lisa Gee <zhusufeng@gmail.com>
2017-11-30 09:05:54 -0800

    Remove temp array and post to DB

--- server/routes/gas.js
+++ server/routes/gas.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const Gas = require('../db/models/gas');
 
 // Temp array until db is used
-let gaslog = [
-  {
-    date: '10/19/2017',
-    previousMileage: 99000,
-    currentMileage: 99500,
-    milesDriven: 500,
-    gallons: 15,
-    MPG: 33.33,
-    pricePerGallon: 3.25,
-    totalSpent: 48.75
-  }
-];
+// let gaslog = [
+//   {
+//     date: '10/19/2017',
+//     previousMileage: 99000,
+//     currentMileage: 99500,
+//     milesDriven: 500,
+//     gallons: 15,
+//     MPG: 33.33,
+//     pricePerGallon: 3.25,
+//     totalSpent: 48.75
+//   }
+// ];
 
 /* GET /gas route to retrieve all gas entries */
 const getGas = (req, res) => {
@@ -22,7 +22,17 @@ const getGas = (req, res) => {
 
 /* POST /gas route to add a gas entry */
 const postGas = (req, res) => {
-  gaslog.push(req.body);
+  var newEntry = new Gas({
+    date: req.body.date,
+    previousMileage: req.body.previousMileage,
+    currentMileage: req.body.currentMileage,
+    milesDriven: req.body.milesDriven,
+    gallons: req.body.gallons,
+    MPG: req.body.MPG,
+    pricePerGallon: req.body.pricePerGallon,
+    totalSpent: req.body.totalSpent
+  });
+  // gaslog.push(req.body);
   res.status(200).send(req.body);
 };
 
