9dcb0761a65580a00a1988175fe87cc36359d450
Lisa Gee <zhusufeng@gmail.com>
2017-12-04 17:01:38 -0800

    Fill out a comment about each endpt

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,15 @@ let users = [];
 
 /* GET /user route to retrieve all user entries */
 // getUsers
+
+/* POST /user route to post a user */
 // postUser
+
+/* GET /user/:id route to retrieve specific user info */
 // getUser
+
+/* PUT /user/:id route to update specific user info */
 // putUser
+
+/* DELETE /user/:id route to delete specific user */
 // deleteUser
