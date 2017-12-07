e321a73a7b8530b41ed556ad424ec96b7ffbba79
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 20:26:41 -0800

    Restructure userService to fit service entry pt syntax

--- client/app/services/userService.js
+++ client/app/services/userService.js
@@ -1,4 +1,12 @@
-angular.module('index')
-.service('userService', function($http) {
+'use strict';
+// angular.module('index')
+// .service('userService', function($http) {
   
-});
+// });
+
+userService.$inject = ['$http'];
+function userService($http) {
+
+}
+
+module.exports = userService;
