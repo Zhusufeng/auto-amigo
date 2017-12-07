0b88e6ed3adf1c49366778e840adb9506e558786
Lisa Gee <zhusufeng@gmail.com>
2017-12-07 09:21:48 -0800

    Revert components/index.js for modularity

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -2,9 +2,9 @@
 
 const angular = require('angular');
 
-// angular.module('index').component('gas', require('./gas'));
-angular.module('index').component('gas', {
-  controller: 'gasCtrl',
-  template: "<span>Hewwwwo</span>"
-  // templateUrl: '../views/gas.html' // this is based on where index.html is located
-});
+angular.module('index').component('gas', require('./gas'));
+// angular.module('index').component('gas', {
+//   controller: 'gasCtrl',
+//   template: "<span>Hewwwwo</span>"
+//   // templateUrl: '../views/gas.html' // this is based on where index.html is located
+// });
