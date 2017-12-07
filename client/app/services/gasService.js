b7090cd80eb5c8ebc10c3a425a458eee5f03a242
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 20:25:08 -0800

    Update gasService to fit syntax for entry pts

--- client/app/services/gasService.js
+++ client/app/services/gasService.js
@@ -1,33 +1,40 @@
-angular.module('index')
-  .service('gasService', function($http, $q) {
+'use strict';
 
-    this.getGasData = function(){
-      const defer = $q.defer();
-      $http.get('/gas')
-        .then((res => {
-          console.log('Success from getGasData!', res.data);
-          defer.resolve(res.data);
-        }),
-        (err => {
-          console.log('Err from getGasData!', err);
-          defer.resolve(err);
-        })
-      );
-      return defer.promise;
-    };
+// angular.module('index')
+//   .service('gasService', function($http, $q) {
 
-    this.postGasData = function(data) {
-      const defer = $q.defer();
-      $http.post('/gas', data)
-        .then((res => {
-          console.log('Success from postGasData!', res);
-          defer.resolve(res);
-        }),
-        (err => {
-          console.log('Err from postGasData!', err);
-          defer.resolve(err);
-        })
-      );
-      return defer.promise;
-    }
-  });
+gasService.$inject = ['$http', '$q'];
+function gasService($http, $q) {
+  this.getGasData = function(){
+    const defer = $q.defer();
+    $http.get('/gas')
+      .then((res => {
+        console.log('Success from getGasData!', res.data);
+        defer.resolve(res.data);
+      }),
+      (err => {
+        console.log('Err from getGasData!', err);
+        defer.resolve(err);
+      })
+    );
+    return defer.promise;
+  };
+
+  this.postGasData = function(data) {
+    const defer = $q.defer();
+    $http.post('/gas', data)
+      .then((res => {
+        console.log('Success from postGasData!', res);
+        defer.resolve(res);
+      }),
+      (err => {
+        console.log('Err from postGasData!', err);
+        defer.resolve(err);
+      })
+    );
+    return defer.promise;
+  }
+}
+  // });
+
+  module.exports = gasService;
