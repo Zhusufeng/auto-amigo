cb263e106dec6f73790cdd3cda32c15f536bbaed
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 20:06:01 -0800

    Update webpack config to bundle vendor code

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,19 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+  context: __dirname + '/client/app',
   entry: {
-    app: './client/app/app.js',
+    app: './app.js',
     vendor: ['angular']
   },
   output: {
     filename: 'app.bundle.js',
     path: __dirname + 'client/js'
-  }
-}
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js'
+    })
+  ]
+};
