e48efefafa6750e651b6411352a2a0c62678b675
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 20:24:31 -0800

    Add entry pt for services

---
+++ client/app/services/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var angular = require('angular');
+
+angular.module('index').service('gasService', require('./gasService'));
+angular.module('index').service('userService', require('./userService'));
