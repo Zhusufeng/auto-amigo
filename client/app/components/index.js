00731d43ee6d93444d399e6c9f63941f4f665307
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 21:14:02 -0800

    Get gas component template to render

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -2,4 +2,9 @@
 
 const angular = require('angular');
 
-angular.module('index').component('gas', require('./gas'));
+// angular.module('index').component('gas', require('./gas'));
+angular.module('index').component('gas', {
+  controller: 'gasCtrl',
+  template: "<span>Hewwwwo</span>"
+  // templateUrl: '../../client/views/gas.html' // this is based on where index.html is located
+});
