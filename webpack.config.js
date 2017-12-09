299167fc9a8913463a76b8cccea0d74b257bf037
Lisa Gee <zhusufeng@gmail.com>
2017-12-09 15:45:07 -0800

    Add module with loader for html

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
     filename: 'app.bundle.js',
     path: __dirname + '/client/js'
   },
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: "html" }
+    ]
+  },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
