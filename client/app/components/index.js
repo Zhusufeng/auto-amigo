0b18f7cadc7ea864fd62d8d7312c35b29f77bf30
Lisa Gee <zhusufeng@gmail.com>
2017-12-09 15:45:23 -0800

    Use template and require for the gas view html

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -2,9 +2,11 @@
 
 const angular = require('angular');
 
-angular.module('index').component('gas', require('./gas'));
-// angular.module('index').component('gas', {
-//   controller: 'gasCtrl',
-//   template: "<span>Hewwwwo</span>"
-//   // templateUrl: '../views/gas.html' // this is based on where index.html is located
-// });
+// angular.module('index').component('gas', require('./gas'));
+angular.module('index').component('gas', {
+  controller: 'gasCtrl',
+  // template: "<span>Hewwwwo</span>"
+  // templateUrl: '../views/gas.html' // this is based on where index.html is located, worked with webpack-dev-server when went to /client/js
+  template: require('../views/gas.html')
+  
+});
