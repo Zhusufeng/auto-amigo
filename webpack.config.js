0f673ba161393bac15902c2f76f73eab028b9df0
Lisa Gee <zhusufeng@gmail.com>
2017-12-12 09:33:16 -0800

    Update and simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 const webpack = require('webpack');
 
 module.exports = {
-  context: __dirname + '/client/app',
-  entry: {
-    app: './app.js',
-    vendor: ['angular']
-  },
+  // context: __dirname + '/client/app',
+  // entry: {
+    // app: './app.js',
+    // vendor: ['angular']
+  // },
+  entry: './client/app/app.js',
   output: {
+    path: __dirname + '/client/js',
     filename: 'app.bundle.js',
-    path: __dirname + '/client/js'
   },
   module: {
     loaders: [
-      { test: /\.html$/, loader: "html" }
+      { test: /\.html$/, loader: 'html-loader' }
     ]
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.bundle.js'
-    })
-  ]
+  // plugins: [
+  //   new webpack.optimize.CommonsChunkPlugin({
+  //     name: 'vendor',
+  //     filename: 'vendor.bundle.js'
+  //   })
+  // ]
 };
