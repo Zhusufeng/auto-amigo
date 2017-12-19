5592571f188e25d0434553c5d0c0e7d3f3af612d
Lisa Gee <zhusufeng@gmail.com>
2017-12-19 10:24:10 -0800

    Require routes as part of app, set as config

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,9 @@
 const angular = require('angular');
 const uirouter = require('angular-ui-router');
+const routes = require('./routes');
 
-angular.module('index', [uirouter]);
+angular.module('index', [uirouter])
+  .config(routes);
 
 require('./components');
 require('./controllers');
