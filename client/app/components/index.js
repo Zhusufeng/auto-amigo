eaaeaf25f9177ca0e1e5bdf7645dbac48e9f27d7
Lisa Gee <zhusufeng@gmail.com>
2017-12-19 10:56:57 -0800

    Add splash component to components/index.js

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -2,6 +2,7 @@
 
 const angular = require('angular');
 
+angular.module('index').component('splash', require('./splash'));
 angular.module('index').component('login', require('./login'));
 angular.module('index').component('signup', require('./signup'));
 angular.module('index').component('gas', {
