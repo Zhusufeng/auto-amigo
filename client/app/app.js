f6341cba4e91fd5eec8ca53cadaa1cc49aeb1992
Lisa Gee <zhusufeng@gmail.com>
2017-12-19 10:08:57 -0800

    Import angular-ui-router to app

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,7 @@
 const angular = require('angular');
+import uirouter from 'angular-ui-router';
 
-angular.module('index', []);
+angular.module('index', [uirouter]);
 
 require('./components');
 require('./controllers');
