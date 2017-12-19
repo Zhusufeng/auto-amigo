af55ccc9612ac96db0252ebef2c2a9b0dbc11589
Lisa Gee <zhusufeng@gmail.com>
2017-12-19 10:55:50 -0800

    Add login and signup to components/index.js

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -2,7 +2,8 @@
 
 const angular = require('angular');
 
-// angular.module('index').component('gas', require('./gas'));
+angular.module('index').component('login', require('./login'));
+angular.module('index').component('signup', require('./signup'));
 angular.module('index').component('gas', {
   controller: 'gasCtrl',
   // template: "<span>Hewwwwo</span>"
