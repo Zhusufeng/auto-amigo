154a3ef51545a2b9b71e924ef1f09817af122ad5
Lisa Gee <zhusufeng@gmail.com>
2017-12-20 10:09:11 -0800

    Place basic functionality of loginCtrl for now

--- client/app/controllers/loginCtrl.js
+++ client/app/controllers/loginCtrl.js
@@ -1,9 +1,11 @@
 function loginCtrl(){
-  this.email = '';
-  this.password = '';
 
-  this.submitLogin = function(e) {
-    console.log(e);
+  this.submitLogin = function() {
+
+    const email = document.querySelector('[name=email]').value;
+    const password = document.querySelector('[name=password]').value;
+
+    console.log(`You hit submit with email ${email} and password ${password}`);
   }
 }
 
