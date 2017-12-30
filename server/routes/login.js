9bc1c7406474644604781ab1c696543912daad13
Lisa Gee <zhusufeng@gmail.com>
2017-12-30 13:47:36 -0800

    Code login route to find a username in User collection

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,8 +1,17 @@
 const db = require('./db/index.js');
 const User = require('../db/models/user');
 
+/* POST /login route to find or create a User entry */
 const postLogin = (req, res) => {
-  
+  console.log(req);
+
+  let name = req.body.username;
+
+  User.findOne({username: name}, function(err, user) {
+    if (err) console.error('Error in User.find: ', err);
+    console.log(`user was found: ${user}`);
+    res.status(200).send(user);
+  })
 };
 
 module.exports = postLogin;
