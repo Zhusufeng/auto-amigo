fe98b8f0a63a42158304ca3116403fabdc348ede
Lisa Gee <zhusufeng@gmail.com>
2017-12-30 14:01:24 -0800

    Add authService to services/index.js

--- client/app/services/index.js
+++ client/app/services/index.js
@@ -2,5 +2,6 @@
 
 var angular = require('angular');
 
+angular.module('index').service('authService', require('./authService'));
 angular.module('index').service('gasService', require('./gasService'));
 angular.module('index').service('userService', require('./userService'));
