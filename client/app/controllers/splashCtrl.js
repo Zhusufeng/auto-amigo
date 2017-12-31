63204c6abaa851691765d3a974189db404a23250
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 11:48:41 -0800

    Update splashCtrl to redirect to / when log out is clicked

--- client/app/controllers/splashCtrl.js
+++ client/app/controllers/splashCtrl.js
@@ -1,8 +1,9 @@
-splashCtrl.$inject = ['authService'];
-function splashCtrl(authService) {
+splashCtrl.$inject = ['authService', '$location'];
+function splashCtrl(authService, $location) {
   this.submit = function() {
     console.log(`You hit log out!`);
     authService.postLogout();
+    $location.path('/');
   };
 }
 
