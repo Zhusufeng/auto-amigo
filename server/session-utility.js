45c4fa3039958bb4b444efb7594dd130c3ca3aec
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 00:08:57 -0800

    Add func to check if authenticated & to create a session

---
+++ server/session-utility.js
@@ -0,0 +1,16 @@
+const checkAuth = (req, res, next) => {
+  if (!req.session.user) {
+    res.status(200).send({error: 'You are not logged in!'}); // TODO: this should redirect to login...
+  } else {
+    next();
+  }
+};
+
+const createSession = (req, res, newUser) => {
+  return req.session.regenerate(() => { // Makes new cookie even if already in a session
+    req.session.user = newUser; // Sets session's user to newest logged in
+    res.status(200).send(newUser); // TODO: this should redirect to main page (gas log for now) 
+  });
+}
+
+module.exports = {};
