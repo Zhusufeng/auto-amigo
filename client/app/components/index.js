3ec805ad2187a77045244d2ecb0fad44c3b069d4
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 02:32:07 -0800

    Update how gasCtrl is required by components/index.js

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -5,10 +5,11 @@ const angular = require('angular');
 angular.module('index').component('splash', require('./splash'));
 angular.module('index').component('login', require('./login'));
 angular.module('index').component('signup', require('./signup'));
-angular.module('index').component('gas', {
-  controller: 'gasCtrl',
-  // template: "<span>Hewwwwo</span>"
-  // templateUrl: '../views/gas.html' // this is based on where index.html is located, worked with webpack-dev-server when went to /client/js
-  template: require('./views/gas.html') // path based on index.js, to get to work with html loader
+angular.module('index').component('gas', require('./gas'));
+// angular.module('index').component('gas', {
+//   controller: 'gasCtrl',
+//   // template: "<span>Hewwwwo</span>"
+//   // templateUrl: '../views/gas.html' // this is based on where index.html is located, worked with webpack-dev-server when went to /client/js
+//   template: require('./views/gas.html') // path based on index.js, to get to work with html loader
   
-});
+// });
