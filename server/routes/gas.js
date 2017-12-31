8863171f4d970845bd56c6e88845bb0e1582cd7e
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 01:14:42 -0800

    Add getUsersGas function to gas route

--- server/routes/gas.js
+++ server/routes/gas.js
@@ -1,6 +1,56 @@
-const mongoose = require('mongoose');
+// Require database models
+const User = require('../db/models/user');
 const Gas = require('../db/models/gas');
 
+// Require session methods
+const util = require('../session-utility');
+
+/* GET /gas route to retrieve a user's gas entries */
+const getUsersGas = (req, res) => {
+  console.log(`req.session is ${req.session}`);
+  console.log(`req.session._id is ${req.session._id}`);
+
+  if (!req.session._id) {
+    res.status(200).send({error: 'Not logged in'});
+  } else {
+    // Create query to find if username is in the database
+    let findQuery = User.findOne({_id: req.session._id});
+
+    // Execute query
+    findQuery.exec(function(err, user) {
+      if (err) console.error('Error in User.findOne: ', err);
+    })
+    .then((userInfo) => {
+      console.log(`userInfo is ${userInfo}`);
+      // Send array of gas entries to client
+      res.status(200).send(userInfo.gaslog);
+    });
+  }
+};
+
+/* POST /gas route to add a gas entry */
+const postGas = (req, res) => {
+  // Create new gasEntry
+  var gasEntry = new Gas({
+    date: req.body.date,
+    previousMileage: req.body.previousMileage,
+    currentMileage: req.body.currentMileage,
+    milesDriven: req.body.milesDriven,
+    gallons: req.body.gallons,
+    MPG: req.body.MPG,
+    pricePerGallon: req.body.pricePerGallon,
+    totalSpent: req.body.totalSpent
+  });
+  
+  // Save gasEntry to database
+  gasEntry.save(function (err, gasEntry) {
+    if (err) return console.error(err);
+    res.status(200).send(req.body);
+  }); 
+};
+
+/* Below functions are now deprecated but can be useful for debugging */
+/* They can be used to save entries straight to the Gas collection */
 /* GET /gas route to retrieve all gas entries */
 const getGas = (req, res) => {
   Gas.find(function(err, gasEntries) {
@@ -31,4 +81,9 @@ const postGas = (req, res) => {
 };
 
 // Export functions using es6
-module.exports = {getGas, postGas};
+module.exports = {
+  getUsersGas,
+  postUsersGas,
+  getGas, 
+  postGas
+};
