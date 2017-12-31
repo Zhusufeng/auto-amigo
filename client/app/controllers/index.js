dd0c52a874b75ca8220ae152c5ff9b82b2ac8fd7
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 00:52:37 -0800

    Add splashCtrl as dependency to index for webpack to bundle

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -4,3 +4,4 @@ const angular = require('angular');
 
 angular.module('index').controller('gasCtrl', require('./gasCtrl'));
 angular.module('index').controller('loginCtrl', require('./loginCtrl'));
+angular.module('index').controller('splashCtrl', require('./splashCtrl'));
