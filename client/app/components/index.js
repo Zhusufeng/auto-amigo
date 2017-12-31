3c31e426ad5b7d0b9808df4456c5cd33bc7f7d96
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 04:04:12 -0800

    Add navbar component to index & alphabetize requirements

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -2,7 +2,8 @@
 
 const angular = require('angular');
 
-angular.module('index').component('splash', require('./splash'));
-angular.module('index').component('login', require('./login'));
-angular.module('index').component('signup', require('./signup'));
 angular.module('index').component('gas', require('./gas'));
+angular.module('index').component('login', require('./login'));
+angular.module('index').component('navbar', require('./navbar'));
+angular.module('index').component('signup', require('./signup'));
+angular.module('index').component('splash', require('./splash'));
