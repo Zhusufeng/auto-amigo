6f14af675c893020728ae79e3c77451fb861f87b
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 00:34:05 -0800

    Clean up some code in login route

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,10 +7,7 @@ const util = require('../session-utility');
 /* POST /login route to find or create a User entry */
 const postLogin = (req, res) => {
   console.log(req.body);
-
   let name = req.body.email;
-
-  // let isFound = false;
 
   // Create query to find if username is in the database
   let findQuery = User.findOne({username: name});
@@ -19,16 +16,13 @@ const postLogin = (req, res) => {
   findQuery.exec(function(err, user) {
     if (err) console.error('Error in User.findOne: ', err);
     console.log(`user was found: ${user}`);
-    // if (user) isFound = true;
   })
-  .then(data => {
-    console.log('Here is the data passed from findQuery.exec', data);
-    // console.log('isFound is ', isFound);
+  .then(userInfo => {
+    console.log('Here is the userInfo passed from findQuery.exec', userInfo);
 
-    if (data) {
+    if (userInfo) {
       // Create session
-      util.createSession(req, res, data);
-      // res.status(200).send(data);
+      util.createSession(req, res, userInfo);
     } else {
       // Create user
       let newUser = new User({ username: name});
@@ -38,7 +32,6 @@ const postLogin = (req, res) => {
         if (err) console.error('Error in newUser.save: ', err);
         console.log(newUser);
         util.createSession(req, res, newUser);        
-        // res.status(201).send(newUser);
       });
     }
   }); 
