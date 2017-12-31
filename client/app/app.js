b722bb6bee0c9688d9fb0aca283d6d08e2f3269f
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 02:40:43 -0800

    Require bootstrap in entry pt on client side

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,6 @@
 const angular = require('angular');
 const uirouter = require('angular-ui-router');
+const bootstrap = require('bootstrap');
 const routes = require('./routes');
 
 angular.module('index', [uirouter])
