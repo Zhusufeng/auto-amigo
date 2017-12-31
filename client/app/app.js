d5e01fbcd1d5f6e81626874cdcf93123970c6de3
Lisa Gee <zhusufeng@gmail.com>
2017-12-31 02:47:03 -0800

    Require Bootstrap's ready-to-use CSS in webpack entry pt

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,7 @@
 const angular = require('angular');
 const uirouter = require('angular-ui-router');
 const bootstrap = require('bootstrap');
+const bootstrapCSS = require('bootstrap/dist/css/bootstrap.min.css');
 const routes = require('./routes');
 
 angular.module('index', [uirouter])
